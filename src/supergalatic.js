098fd9a9a3a23bf6b2e232bd1f27512830b425d1
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2018-10-12 16:46:34 -0700

    it will attempt a few functions

--- src/supergalatic.js
+++ src/supergalatic.js
@@ -1,7 +1,11 @@
 export class GalaticAge {
-    constructor(year) {
+    constructor(year, age) {
     this.year = year;
-    
+    this.mercuryAge = //365 * age / 87.6
+    this.venusAge = // 365 * age / 224.7
+    this.marsAge = //365 * age / 687
+    this.jupiterAge = //365 * age / 4332.6
+  
     }
     findYear(){
         let newGalaticYear = new Date(this.year);
@@ -9,11 +13,23 @@ export class GalaticAge {
         return newYear;
     }
     findMercury(){
-        let newMercury = new Date(this.year);
-        let newMercuryAge = newMercury.getFullYear();
-        console.log(newMercuryAge * 30 / 87.6);
-        let getAge = newMercuryAge * 30 / 87.6;
+        return
+        // const mercury = ;
+        // let newMercury = new Date(this.year);
+        // let newMercuryAge = newMercury.getFullYear();
+        // console.log(newMercuryAge * 30 / 87.6);
+        // let getAge = newMercuryAge * 30 / 87.6;
         
-        return getAge;
+        // return getAge;
     }
+    findVenus(){
+        return ;
+    }
+    findMars(){
+        return ;
+    }
+    findJupiterAge() {
+        return ;
+    }
+    
 }
