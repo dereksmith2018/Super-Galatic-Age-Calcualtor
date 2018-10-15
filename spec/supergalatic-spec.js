101e09e552fb53f890df7cf712add76aa6cdb156
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2018-10-15 13:23:49 -0700

    it will add more tests to backend page

--- spec/supergalatic-spec.js
+++ spec/supergalatic-spec.js
@@ -14,9 +14,25 @@ describe('GalaticAge', function(){
     var marsAge = new GalaticAge (18);
     expect(marsAge.findMars()).toEqual(9);
   });
-  it('return the age on mercury that was inputted', function(){
+  it('return the age on jupiter that was inputted', function(){
     var jupiterAge = new GalaticAge (18);
     expect(jupiterAge.findJupiter()).toEqual(1);
   });
-
+  it('return the time for mercury that user has left to live', function(){
+    var mercuryAge = new GalaticAge (18);
+    expect(mercuryAge.timeLeftMercury()).toEqual(258);
+  });
+  it('return the time for venus that user has left to live', function(){
+    var venusAge = new GalaticAge (18);
+    expect(venusAge.timeLeftVenus()).toEqual(100);
+  });
+  it('return the time for mars that user has left to live', function(){
+    var marsAge = new GalaticAge (18);
+    expect(marsAge.timeLeftMars()).toEqual(33);
+  });
+  it('return the time for jupiter that user has left to live', function(){
+    var jupiterAge = new GalaticAge (18);
+    expect(jupiterAge.timeLeftJupiter()).toEqual(5);
+  });
+  
 });
